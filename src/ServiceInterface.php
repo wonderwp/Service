af9d489c9cf2a1ae92cc18b7ab0410a3d97c0e2e
Jeremy Desvaux <jeremy@jdmweb.com>
2025-07-02 11:48:40 +0200

    feat(bbs): added block bindings source const

--- src/ServiceInterface.php
+++ src/ServiceInterface.php
@@ -4,25 +4,26 @@ namespace WonderWp\Component\Service;
 
 interface ServiceInterface
 {
-    const ACTIVATOR_NAME                = 'activator';
-    const API_SERVICE_NAME              = 'api';
-    const ASSETS_SERVICE_NAME           = 'assets';
-    const BLOCK_STYLE_SERVICE_NAME      = 'blockStyle';
-    const BLOCK_TYPE_SERVICE_NAME       = 'blockType';
-    const BLOCK_VARIATION_SERVICE_NAME  = 'blockVariation';
-    const COMMAND_SERVICE_NAME          = 'command';
-    const CUSTOM_POST_TYPE_SERVICE_NAME = 'custom_post_type';
-    const CUSTOM_FIELDS_SERVICE_NAME    = 'custom_fields';
-    const DEACTIVATOR_NAME              = 'deactivator';
-    const FILTER_SERVICE_NAME           = 'filter';
-    const HOOK_SERVICE_NAME             = 'hooks';
-    const LIST_TABLE_SERVICE_NAME       = 'listTable';
-    const MODEL_FORM_SERVICE_NAME       = 'modelForm';
-    const PAGINATION_SERVICE_NAME       = 'pagination';
-    const REPOSITORY_SERVICE_NAME       = 'repository';
-    const ROUTE_SERVICE_NAME            = 'route';
-    const SEARCH_SERVICE_NAME           = 'search';
-    const SHORT_CODE_SERVICE_NAME       = 'shortCode';
-    const TAXONOMY_SERVICE_NAME         = 'taxonomy';
-    const VIEW_SERVICE_NAME             = 'view';
+    const ACTIVATOR_NAME                        = 'activator';
+    const API_SERVICE_NAME                      = 'api';
+    const ASSETS_SERVICE_NAME                   = 'assets';
+    const BLOCK_STYLE_SERVICE_NAME              = 'blockStyle';
+    const BLOCK_TYPE_SERVICE_NAME               = 'blockType';
+    const BLOCK_VARIATION_SERVICE_NAME          = 'blockVariation';
+    const BLOCK_BINDINGS_SOURCE_SERVICE_NAME    = 'bindingSource';
+    const COMMAND_SERVICE_NAME                  = 'command';
+    const CUSTOM_POST_TYPE_SERVICE_NAME         = 'custom_post_type';
+    const CUSTOM_FIELDS_SERVICE_NAME            = 'custom_fields';
+    const DEACTIVATOR_NAME                      = 'deactivator';
+    const FILTER_SERVICE_NAME                   = 'filter';
+    const HOOK_SERVICE_NAME                     = 'hooks';
+    const LIST_TABLE_SERVICE_NAME               = 'listTable';
+    const MODEL_FORM_SERVICE_NAME               = 'modelForm';
+    const PAGINATION_SERVICE_NAME               = 'pagination';
+    const REPOSITORY_SERVICE_NAME               = 'repository';
+    const ROUTE_SERVICE_NAME                    = 'route';
+    const SEARCH_SERVICE_NAME                   = 'search';
+    const SHORT_CODE_SERVICE_NAME               = 'shortCode';
+    const TAXONOMY_SERVICE_NAME                 = 'taxonomy';
+    const VIEW_SERVICE_NAME                     = 'view';
 }
